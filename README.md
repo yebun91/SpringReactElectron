8cd621def3e9418be34579717ea70ab50a7eeb7e
yjchoi <yjchoi@unisemiot.com>
2022-05-10 09:04:16 +0900

    readme 추가

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # SpringReactElectron
+
+1. $ cd frontend 로 이동한 후  yarn add --dev concurrently wait-on 명령어로 추가
+2. npm start
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,33 +6,33 @@ function App() {
   const [message, setMessage]=useState([]);
   useEffect(()=>{
     fetch("/hello")
-        .then((res)=>{
-          return res.json();
-        })
-        .then((data)=>{
-          setMessage(data);
-        });
+      .then((res)=>{
+        return res.json();
+      })
+      .then((data)=>{
+        setMessage(data);
+      });
   },[]);
   return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <ul>
-            {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
-          </ul>
-        </header>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+        <ul>
+          {message.map((v,idx)=><li key={`${idx}-${v}`}>{v}</li>)}
+        </ul>
+      </header>
+    </div>
   );
 }
 
