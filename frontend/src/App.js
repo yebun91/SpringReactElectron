ca780e098aff6020b57326b1a2bc24d7711bb192
yjchoi <yjchoi@unisemiot.com>
2022-05-11 15:57:21 +0900

    일렉트론용으로 초기화

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,7 @@
-import "./styles/App.css";
-import React, { Component } from "react";
-import LifeCycleSample from "./components/LifeCycleSample";
-import ErrorBoundary from "./components/ErrorBoundary";
+import React from "react";
 
-function getRandomColor() {
-  return "#" + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  state = {
-    color: "#000000",
-  };
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>랜덤생성</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return <div>나와라 오바</div>;
+};
 
 export default App;
